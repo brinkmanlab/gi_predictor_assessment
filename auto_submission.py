619bab00c55f5412503fbdea7c3fabb506317f2d
BeefTron <beeftilley@gmail.com>
2017-05-25 18:42:43 -0700

    Improved sloppy code

--- auto_submission.py
+++ auto_submission.py
@@ -11,36 +11,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import selenium.common.exceptions as e
 from selenium.webdriver.support import expected_conditions as EC
-import os, time, io
+import os, time, io, subprocess
 
-# function to automate submission, download and renaming/moving of results for
-# PredictBias. Will only work on my computer without changes
-def predictbias_submit(gbk_path, genome):
+# automated submission, download and renaming/moving of result
+def predictbias_submit(gbk_dir, genome):
     print("Running genome "+genome)
     driver = webdriver.Chrome() # change for different browsers
     driver.get("http://www.bioinformatics.org/sachbinfo/predictbias.html")
     assert "PredictBias" in driver.title
     print("Uploading genbank file...")
     upload = driver.find_element_by_name("brwGenome")
-    upload.send_keys(gbk_path+genome+".gbk")
+    upload.send_keys(os.path.abspath(gbk_dir+genome+".gbk"))
     submit = driver.find_element_by_name("btnSubmit")
     submit.send_keys(Keys.RETURN)
     print("Downloading results...")
     driver.get(driver.current_url)
     download = driver.find_element_by_link_text("download")
-    download.send_keys(Keys.RETURN)
-    # The following code is to rename the download to the proper label and move
-    # to my predictbias folder. Will only work on my system with an empty dowload folder.
-    time.sleep(6) # allow time for download
-    result_file = os.listdir("/home/beef/Downloads/")
-    if len(result_file) == 1:
-        os.rename("/home/beef/Downloads/"+result_file[0], "/home/beef/Documents/BrinkmanLab/GenomicIslands/PredictBias/"+genome+".tar.gz")
-    elif len(result_file) > 1:
-        print("Multiple files in downloads - result not renamed/moved")
-    else:
-        # When PredictBias fails to find any genomic islands, the download link
-        # takes browser to bioinformatics main page - nothing downloaded.
-        print("GENOME "+genome+" FAILED")
+    link = download.get_attribute("href")
+    subprocess.Popen(["wget",link])
+    time.sleep(6) # allow time to download
+    subprocess.Popen(["mv", link.split("/")[-1], genome+".tar.gz"])
     driver.close()
     
 def paidb_submit(ffnfile, outhtml):
